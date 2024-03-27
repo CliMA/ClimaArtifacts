a748b7886318322fbbf44b65145e1a6fd57fe4f4
Gabriele Bozzola <sbozzolator@gmail.com>
2024-03-27 09:46:10 -0700

    Add undownloadable artifacts

--- ClimaArtifactsHelper.jl/src/ClimaArtifactsHelper.jl
+++ ClimaArtifactsHelper.jl/src/ClimaArtifactsHelper.jl
@@ -2,16 +2,30 @@ module ClimaArtifactsHelper
 
 using ArtifactUtils
 using REPL.TerminalMenus
-
 using Pkg.Artifacts
 
-export create_add_artifact_guided
+import SHA: sha1
+
+export create_artifact_guided
 
 const MB = 1024 * 1024 * 1024
 
 # Mark files larger than this as "undownloadable". This will prevent them from
 # being mirrored by the Julia servers
 const LARGE_FILESIZE = 500MB
+
+"""
+    foldersize(dir = ".")
+
+Recursively walk dir and find the total size of all the files.
+"""
+function foldersize(dir = ".")
+    size = 0
+    for (root, dirs, files) in walkdir(dir)
+        size += sum(map(filesize, joinpath.(root, files)))
+    end
+    return size
+end
 
 """
     add_tar_gz_to_path(path)
@@ -39,19 +53,25 @@ function create_tarball(
     return tar_path
 end
 
-"""
-    create_add_artifact_guided(artifact_dir)
+function _recommend_uploading_to_cluster(hash, artifact_name, artifact_dir)
+    println("The id of your artifact is $hash")
+    println(
+        "Create the folder `/groups/esm/artifacts/$artifact_name` on the cluster",
+    )
+    println("Then, upload the content of $artifact_dir to that folder")
+    println(
+        "Add the following entry to the Overrides.toml file you find in `/groups/esm/ClimaArtifacts/artifacts/`",
+    )
+    println()
+    println("$hash = \"/groups/esm/ClimaArtifacts/artifacts/$artifact_name\"")
+    println()
+end
 
-Start a guided process to create an artifact from a directory of files.
-"""
-function create_add_artifact_guided(artifact_dir; artifact_name)
-    undownloadable = false
-    if filesize(artifact_dir) > LARGE_FILESIZE
-        undownloadable = true
-        error("NOT IMPLEMENTED YET")
-        # TODO: Implement this feature
-    end
-
+function _create_downloadable_artifact(
+    artifact_dir;
+    artifact_name,
+    output_artifacts = "OutputArtifacts.toml",
+)
     println("First, we will create the artifact tarball")
     println(
         "You will upload it to the Caltech Data Archive and provide the direct link",
@@ -71,15 +91,15 @@ function create_add_artifact_guided(artifact_dir; artifact_name)
         "I will try to download your freshly minted artifact to check that it works (might take a while)",
     )
 
-    # This is where we save the string we create
-    output_artifacts = "OutputArtifacts.toml"
-    isfile(output_artifacts) && @warn "Found $output_artifacts. It will be overwritten"
-
     # Bind the artifact to a temporary Artifact.toml, retrieve it, and print it.
     # We crate a temporary Artifact.toml to ensure that it is an empty file.
     mktempdir() do path
         artifact_toml = joinpath(path, "Artifacts.toml")
-        ArtifactUtils.add_artifact!(artifact_toml, artifact_name, tarball_url)
+        hash = ArtifactUtils.add_artifact!(
+            artifact_toml,
+            artifact_name,
+            tarball_url,
+        )
         open(artifact_toml, "r") do file
             artifact_str = read(file, String)
             println(
@@ -88,11 +108,54 @@ function create_add_artifact_guided(artifact_dir; artifact_name)
             println()
             println(artifact_str)
         end
-        Base.mv(artifact_toml, output_artifacts)
+        Base.mv(artifact_toml, output_artifacts, force = true)
+        println("You should also consider uploading your data to the cluster")
+        _recommend_uploading_to_cluster(hash, artifact_name, artifact_dir)
+    end
+end
+
+"""
+    create_artifact_guided(artifact_dir)
+
+Start a guided process to create an artifact from a directory of files.
+"""
+function create_artifact_guided(artifact_dir; artifact_name)
+    output_artifacts = "OutputArtifacts.toml"
+    # This is where we save the string we create
+    isfile(output_artifacts) &&
+        @warn "Found $output_artifacts. It will be overwritten"
+
+    if foldersize(artifact_dir) > LARGE_FILESIZE
+        # Artifacts that are too large. In this case, we have to manually create an hash and
+        # recommend using the Overrides.toml
+        println(
+            "The artifact directory is large ($(foldersize(artifact_dir)) bytes), so the artifact has to be handled manually",
+        )
+
+        # We use the name to create the hash
+        hash = bytes2hex(sha1(artifact_name))
+
+        _recommend_uploading_to_cluster(hash, artifact_name, artifact_dir)
+        println(
+            "Here is your artifact string. Copy and past it to your Artifacts.toml",
+        )
+        println()
+        artifacts_str = "[$artifact_name]\ngit-tree-sha1 = \"$hash\"\n"
+        println(artifacts_str)
+
+        open(output_artifacts, "w") do file
+            write(file, artifacts_str)
+        end
+    else
+        _create_downloadable_artifact(
+            artifact_dir;
+            artifact_name,
+            output_artifacts,
+        )
     end
 
     println("Artifact string saved to $output_artifacts")
-    println("Feel free to add other metadata and make it lazy")
+    println("Feel free to add other metadata/properties (e.g., laziness)")
     println("Enjoy the rest of your day!")
 end
 
