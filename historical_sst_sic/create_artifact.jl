c49f473ccdafbccf15d38494ed6042d7556aaa74
Gabriele Bozzola <sbozzolator@gmail.com>
2024-09-26 13:49:20 -0700

    Add lowres version of historical_sst_sic
    The hash has changed for the higher res version because the previous
    hash was incorrect. We can keep using the previous hash thanks to the
    Overrides

--- historical_sst_sic/create_artifact.jl
+++ historical_sst_sic/create_artifact.jl
@@ -1,5 +1,6 @@
 using Downloads
 
+using NCDatasets
 using ClimaArtifactsHelper
 
 # Downloaded from https://gdex.ucar.edu/dataset/158_asphilli.html
@@ -17,10 +18,8 @@ else
     mkdir(output_dir)
 end
 
-for (path, url) in (
-    SIC_FILE_PATH => SIC_FILE_URL,
-    SST_FILE_PATH => SST_FILE_URL
-    )
+for (path, url) in
+    (SIC_FILE_PATH => SIC_FILE_URL, SST_FILE_PATH => SST_FILE_URL)
     if !isfile(path)
         @info "$path not found, downloading it (might take a while)"
         downloaded_file = Downloads.download(url)
@@ -31,3 +30,65 @@ end
 
 @info "Data file generated!"
 create_artifact_guided(output_dir; artifact_name = basename(@__DIR__))
+
+function thin_artifact(
+    filein,
+    fileout,
+    varname;
+    THINNING_FACTOR = 6,
+    MAX_TIME = 12,
+)
+    ncin = NCDataset(filein)
+    ncout = NCDataset(fileout, "c")
+    FT = Float32
+
+    defDim(ncout, "lon", Int(ceil(length(ncin["lon"]) // THINNING_FACTOR)))
+    defDim(ncout, "lat", Int(ceil(length(ncin["lat"]) // THINNING_FACTOR)))
+    defDim(ncout, "time", MAX_TIME)
+
+    lon = defVar(ncout, "lon", FT, ("lon",), attrib = ncin["lon"].attrib)
+    lon[:] = Array(ncin["lon"])[begin:THINNING_FACTOR:end]
+
+    lat = defVar(ncout, "lat", FT, ("lat",), attrib = ncin["lat"].attrib)
+    lat[:] = Array(ncin["lat"])[begin:THINNING_FACTOR:end]
+
+    time_ = defVar(ncout, "time", FT, ("time",), attrib = ncin["time"].attrib)
+    time_[:] = Array(ncin["time"])[begin:MAX_TIME]
+
+    defVar(
+        ncout,
+        varname,
+        FT,
+        ("lon", "lat", "time"),
+        attrib = ncin[varname].attrib,
+    )
+    ncout[varname][:, :, :] = ncin[varname][
+        begin:THINNING_FACTOR:end,
+        begin:THINNING_FACTOR:end,
+        begin:MAX_TIME,
+    ]
+
+    close(ncin)
+    close(ncout)
+end
+
+output_dir_lowres = "historical_sst_sic_lowres"
+if isdir(output_dir_lowres)
+    @warn "$output_dir_lowres already exists. Content will end up in the artifact and may be overwritten."
+    @warn "Abort this calculation, unless you know what you are doing."
+else
+    mkdir(output_dir_lowres)
+end
+
+new_sic_name = replace(SIC_FILE_PATH, ".nc" => "_lowres.nc")
+thin_artifact(
+    SIC_FILE_PATH,
+    joinpath(output_dir_lowres, new_sic_name),
+    "SEAICE",
+)
+
+new_sst_name = replace(SST_FILE_PATH, ".nc" => "_lowres.nc")
+thin_artifact(SST_FILE_PATH, joinpath(output_dir_lowres, new_sst_name), "SST")
+
+@info "Data file generated!"
+create_artifact_guided(output_dir_lowres; artifact_name = basename(@__DIR__) * "_lowres", append = true)
