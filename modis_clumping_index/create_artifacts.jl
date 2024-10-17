6ff165f97c33fe88cd29ae1f889dad7a7a855fbc
Edward Speer <espeer@caltech.edu>
2024-10-17 15:50:30 -0700

    use GriddingMachine

--- modis_clumping_index/create_artifacts.jl
+++ modis_clumping_index/create_artifacts.jl
@@ -3,66 +3,105 @@
 # Global 500-m Foliage Clumping Index Data Derived from MODIS BRDF, 2006.
 # ORNL DAAC, Oak Ridge, Tennessee, USA. https://doi.org/10.3334/ORNLDAAC/1531
 
-# First, download the clumping index data derived from MODIS at 
-# https://daac.ornl.gov/cgi-bin/dsviewer.pl?ds_id=1531 and put the path to that
-# your local copy of that data in the TIFF_PATH constant below.
+# We will use GriddingMachine.jl to fetch and process the MODIS CI data into the
+# desired format for consumption by the Land model:
+# Y. Wang, P. Köhler, R. K. Braghiere, M. Longo, R. Doughty, A. A. Bloom, and
+# C. Frankenberg. 2022. GriddingMachine, a database and software for Earth
+# system modeling at global and regional scales. Scientific Data. 9: 258
+# https://doi.org/10.1038/s41597-022-01346-x
+
+################################################################################
+# IMPORTS                                                                      #
+################################################################################
 
 using NCDatasets
+
+using GriddingMachine.Collector
+using GriddingMachine.Indexer
+using GriddingMachine.Blender
+
 using ClimaArtifactsHelper
 
-TIFF_PATH = missing
+################################################################################
+# CONSTANTS                                                                    #
+################################################################################
 
-outputdir = "clumping_index_artifacts"
-if isdir(outputdir)
-    @warn "$outputdir already exists. Content will end up in the artifact
-            and may be overwritten."
+# Dataset name to be fetched via the GriddingMachine
+MODIS_CI_DATASET = "2X_1Y_V1"
+
+# Output directory
+OUTPUT_DIR = "modis_clumping_index"
+
+# Output nc file name
+OUTPUT_FILE = "He_et_al_2012_1x1.nc"
+
+# Resolution of the simulation
+RESOLUTION = 1
+
+################################################################################
+# MAIN                                                                         #
+################################################################################
+
+if isdir(OUTPUT_DIR)
+    @warn "$OUTPUT_DIR already exists. Content will end up in the artifact and
+           may be overwritten."
     @warn "Abort this calculation, unless you know what you are doing."
 else
-    mkdir(outputdir)
+    mkdir(OUTPUT_DIR)
 end
 
-# Use the process_tiff python script to reproject the geotiff data to lat/long
-# from sinusoidal coordinates and save the result to a netcdf file. The nc file 
-# is now stored in this directory at clumping_index.nc
-@assert(success(`python3 process_tif.py $TIFF_PATH`))
+# Download the CI data as a julia artifact.
+ci_collector  = Collector.clumping_index_collection()
+download_path = Collector.query_collection(ci_collector, MODIS_CI_DATASET)
 
-# Utilities for regridding and working with ncdatasets
-include("utils.jl")
+# Read the data from the dowloaded nc file
+ci_data = Indexer.read_LUT(download_path)
 
-# Get the lat/long from the nc data
-nc_data = NCDatasets.NCDataset("clumping_index.nc")
-lat = nc_data["lat"][:];
-lon = nc_data["lon"][:];
+# Regrid the data to the desired simulation resolution - 1x1 degree. Note that 
+# the regridder will require integer trucation of the divider (1/R)
+ci_data_regridded = Blender.regrid(ci_data[1], Int64(1 / RESOLUTION))
 
-# Empty data array
-data = Array{Union{Missing, Float32}}(missing, length(lon), length(lat))
+# Get the original the lat/lon vectors of the dataset so that we may compute the
+# lat/lon of the regridded data.
+orig_data = NCDataset(download_path, "r")
+orig_lat = orig_data["lat"][:]
+orig_lon = orig_data["lon"][:]
+close(orig_data)
 
-# Simulation Resolution
-resolution = 1.0
+# Since we regridded to half of the resolution of the original data, we need to 
+# average every 2 lat/lon points to get the lat/lon of the regridded data.
+out_lon = (orig_lon[1:2:end] .+ orig_lon[2:2:end]) ./ 2
+out_lat = (orig_lat[1:2:end] .+ orig_lat[2:2:end]) ./ 2
 
-# Function which reads in the data, regrids to the simulation grid, writes the file to the correct output location.
-function create_artifact(data, files, attrib, transform, outfilepath)
-    # get parameter values at each layer
-    read_nc_data!(data, files, filedir)
-    outdata, outlat, outlon =
-        regrid(data, (lon, lat), resolution, transform, nlayers)
-    write_nc_out(outdata, outlat, outlon, z, attrib, outfilepath)
-    Base.mv(outfilepath, joinpath(outputdir, outfilepath))
-end
+# Write the regridded data to the output directory
+output_path = joinpath(OUTPUT_DIR, OUTPUT_FILE)
+ds          = NCDataset(output_path, "c")
 
-# Process clumping index
-files = ["clumping_index.nc"]
+# Define the dimensions of the data -> 2D spatially varying data
+defDim(ds, "lon", size(ci_data_regridded)[1])
+defDim(ds, "lat", size(ci_data_regridded)[2])
 
-transform(x) = x # CI already in correct units
-outfilepath = "clumping_index_map_he_etal2017_$(resolution)x$(resolution)x1.nc"
-attrib = (;
-    vartitle = "Foliage Clumping Index",
-    varunits = "unitless",
-    varname = "ci",
-)
-create_artifact(data, files, attrib, transform, outfilepath)
+# Define the variables of the data - lat, lon, and the clumping index
+la     = defVar(ds, "lat", Float32, ("lat",))
+lo     = defVar(ds, "lon", Float32, ("lon",))
+ci_var = defVar(ds, "ci", Float32, ("lon", "lat"))
 
-create_artifact_guided(outputdir; artifact_name=basename(@__DIR__))
+# Set the attributes of the variables. Clumping index is unitless.
+la.attrib["units"]             = "degrees_north"
+la.attrib["standard_name"]     = "latitude"
+lo.attrib["units"]             = "degrees_east"
+lo.attrib["standard_name"]     = "longitude"
+ci_var.attrib["units"]         = "unitless"
+ci_var.attrib["standard_name"] = "Foliage Clumping Index"
 
-# Now we are done with the intermediate nc file and can remove it
-rm("clumping_index.nc")
+# Write the data for each variable out to the nc file
+la[:]     = out_lat
+lo[:]     = out_lon
+ci_var[:, :] = ci_data_regridded
+close(ds)
+
+# Remove the initial downloaded artifact file - desired data is now stored in 
+# the CliMA artifact.
+Collector.clean_collections!(ci_collector)
+
+create_artifact_guided(OUTPUT_DIR; artifact_name=basename(@__DIR__))
