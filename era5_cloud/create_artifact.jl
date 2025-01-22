591bc9ad7e03623a6e6673a1b5f328fcff49415b
Zhaoyi Shen <11598433+szy21@users.noreply.github.com>
2025-01-21 17:07:30 -0800

    change era5 cloud artifact to hourly data (#98)
    * add hourly era5 cloud data in 2010
    
    * Update era5_cloud/create_artifact.jl
    
    Co-authored-by: Gabriele Bozzola <sbozzolator@gmail.com>
    
    ---------
    
    Co-authored-by: Gabriele Bozzola <sbozzolator@gmail.com>

--- era5_cloud/create_artifact.jl
+++ era5_cloud/create_artifact.jl
@@ -1,70 +1,94 @@
 using NCDatasets
 using ClimaArtifactsHelper
 
-FILE_PATH = "era5_cloud_201001-201012.nc"
-
 const H_EARTH = 7000.0
 const P0 = 1e5
 const HPA_TO_PA = 100.0
 
 Plvl_inv(P) = -H_EARTH * log(P / P0)
 
-function create_cloud(infile_path, outfile_path)
-    ncin = NCDataset(infile_path)
-    ncout = NCDataset(outfile_path, "c")
+short_name_dict = Dict(
+    "fraction_of_cloud_cover" => "cc",
+    "specific_cloud_liquid_water_content" => "clwc",
+    "specific_cloud_ice_water_content" => "ciwc",
+    "specific_humidity" => "q",
+)
 
-    # This artifact is for temporarily prescribing clouds in our simulations, so we
-    # only need a coarse resolution. Here we resample the ERA5 data (0.25x0.25 degrees)
-    # to 2x2 degrees.
-    THINNING_FACTOR = 8
+function create_cloud(variables, outfile_path; small=false)
+
+    ncout = NCDataset(outfile_path, "c")
+    infile_paths = map(m -> "era5_cloud_hourly_$(variables[1])_2010$(m).nc", lpad.(1:12, 2, "0"))
+    length(infile_paths) != 12 && error(
+        "Did not find twelve .nc files for variable $(variables[1]). Rerun Python script or check that all files are downloaded in this directory.",
+    )
+    ncin = NCDataset(infile_paths, aggdim = "valid_time")
+    
     FT = Float32
+    THINNING_FACTOR = small ? 6 : 1
 
     defDim(ncout, "lon", Int(ceil(length(ncin["longitude"]) // THINNING_FACTOR)))
     defDim(ncout, "lat", Int(ceil(length(ncin["latitude"]) // THINNING_FACTOR)))
     defDim(ncout, "z", length(ncin["pressure_level"]))
-    defDim(ncout, "date", length(ncin["date"]))
+    defDim(ncout, "time", Int(ceil(length(ncin["valid_time"]) // THINNING_FACTOR)))
 
     lon_attribs = Dict(ncin["longitude"].attrib)
     lon_attribs["_FillValue"] = NaN32
-    lon = defVar(ncout, "lon", FT, ("lon",), attrib = lon_attribs, deflatelevel = 9)
+    lon = defVar(ncout, "lon", FT, ("lon",), attrib = lon_attribs)
     lon[:] = Array(ncin["longitude"])[begin:THINNING_FACTOR:end]
 
     lat_attribs = Dict(ncin["latitude"].attrib)
     lat_attribs["_FillValue"] = NaN32
-    lat = defVar(ncout, "lat", FT, ("lat",), attrib = lat_attribs, deflatelevel = 9)
+    lat = defVar(ncout, "lat", FT, ("lat",), attrib = lat_attribs)
     # ERA5 latitude coordinate is from 90 to -90, here we reverse it so that we can use ClimaUtilitiles.
     lat[:] = reverse(Array(ncin["latitude"])[begin:THINNING_FACTOR:end])
 
-    z = defVar(ncout, "z", FT, ("z",), deflatelevel = 9)
+    z = defVar(ncout, "z", FT, ("z",))
     plevin = ncin["pressure_level"] .* HPA_TO_PA
     @. z = Plvl_inv(plevin)
     z.attrib["long_name"] = "altitude"
     z.attrib["units"] = "meters"
 
-    date_ = defVar(ncout, "date", Int32, ("date",), attrib = ncin["date"].attrib, deflatelevel = 9)
-    # ERA5 defines monthly mean data on the first day of the month, so we shift it by 14 days.
-    date_[:] = Array(ncin["date"]) .+ Int32(14)
+    time_ = defVar(
+        ncout,
+        "time",
+        Int32,
+        ("time",),
+        attrib = ncin["valid_time"].attrib,
+    )
+    time_[:] = Array(ncin["valid_time"])[begin:THINNING_FACTOR:end]
+    close(ncin)
 
-    cloud_names = ("cc", "clwc", "ciwc")
-    for name in cloud_names
+    attrib_names =
+        ["standard_name", "long_name", "units", "_FillValue", "GRIB_missingValue"]
+    attrib_renames = ["standard_name", "long_name", "units", "_FillValue", "missing_value"]
+    for variable in variables
+        infile_paths = map(m -> "era5_cloud_hourly_$(variable)_2010$(m).nc", lpad.(1:12, 2, "0"))
+        length(infile_paths) != 12 && error(
+            "Did not find twelve .nc files for variable $(variable). Rerun Python script or check that all files are downloaded in this directory.",
+        )
+        ncin = NCDataset(infile_paths, aggdim = "valid_time")
+        name = short_name_dict[variable]
+        attribs = Dict([
+            attrib_rename => ncin[name].attrib[attrib_name] for
+            (attrib_name, attrib_rename) in zip(attrib_names, attrib_renames)
+        ])
         defVar(
             ncout,
             name,
             FT,
-            ("lon", "lat", "z", "date"),
-            attrib = ncin[name].attrib,
-            deflatelevel = 9,
+            ("lon", "lat", "z", "time"),
+            attrib = attribs,
         )
-
-        ncout[name][:,:,:,:] = reverse(ncin[name][begin:THINNING_FACTOR:end,begin:THINNING_FACTOR:end,:,:], dims=2)
+        ncout[name][:,:,:,:] = reverse(ncin[name][begin:THINNING_FACTOR:end,begin:THINNING_FACTOR:end,:,begin:THINNING_FACTOR:end], dims=2)
+        close(ncin)
     end
 
-    close(ncin)
     close(ncout)
 end
 
-output_dir = "era5_cloud_artifact"
+variables = ["fraction_of_cloud_cover", "specific_cloud_liquid_water_content", "specific_cloud_ice_water_content", "specific_humidity"]
 
+output_dir = "era5_cloud_artifact"
 if isdir(output_dir)
     @warn "$output_dir already exists. Content will end up in the artifact and may be overwritten."
     @warn "Abort this calculation, unless you know what you are doing."
@@ -72,12 +96,20 @@ else
     mkdir(output_dir)
 end
 
-if !isfile(FILE_PATH)
-    @info "$FILE_PATH not found, downloading it (might take a while)"
-    run(`python download_era5.py`)
-end
-
-create_cloud(FILE_PATH, joinpath(output_dir, "era5_cloud.nc"))
+create_cloud(variables, joinpath(output_dir, "era5_cloud.nc"))
 
 @info "Data file generated!"
 create_artifact_guided(output_dir; artifact_name = basename(@__DIR__))
+
+output_dir_lowres = "era5_cloud_artifact_lowres"
+if isdir(output_dir_lowres)
+    @warn "$output_dir_lowres already exists. Content will end up in the artifact and may be overwritten."
+    @warn "Abort this calculation, unless you know what you are doing."
+else
+    mkdir(output_dir_lowres)
+end
+
+create_cloud(variables, joinpath(output_dir_lowres, "era5_cloud_lowres.nc"), small=true)
+
+@info "Data file generated!"
+create_artifact_guided(output_dir_lowres; artifact_name = basename(@__DIR__) * "_lowres", append=true)
--- era5_cloud/download_era5.py
+++ era5_cloud/download_era5.py
@@ -1,39 +1,68 @@
 import cdsapi
-
-dataset = "reanalysis-era5-pressure-levels-monthly-means"
-request = {
-    "product_type": ["monthly_averaged_reanalysis"],
-    "variable": [
-        "fraction_of_cloud_cover",
-        "specific_cloud_ice_water_content",
-        "specific_cloud_liquid_water_content"
-    ],
-    "pressure_level": [
-        "1", "2", "3",
-        "5", "7", "10",
-        "20", "30", "50",
-        "70", "100", "125",
-        "150", "175", "200",
-        "225", "250", "300",
-        "350", "400", "450",
-        "500", "550", "600",
-        "650", "700", "750",
-        "775", "800", "825",
-        "850", "875", "900",
-        "925", "950", "975",
-        "1000"
-    ],
-    "year": ["2010"],
-    "month": [
-        "01", "02", "03",
-        "04", "05", "06",
-        "07", "08", "09",
-        "10", "11", "12"
-    ],
-    "time": ["00:00"],
-    "data_format": "netcdf",
-    "download_format": "unarchived"
-}
+import concurrent.futures
+import itertools
 
 client = cdsapi.Client()
-client.retrieve(dataset, request, "era5_cloud_201001-201012.nc")
+dataset = "reanalysis-era5-pressure-levels"
+
+def download_era5(variable, month):
+    request = {
+        "product_type": ["reanalysis"],
+        "variable": variable,
+        "year": ["2010"],
+        "month": month,
+        "day": [
+            "01", "02", "03",
+            "04", "05", "06",
+            "07", "08", "09",
+            "10", "11", "12",
+            "13", "14", "15",
+            "16", "17", "18",
+            "19", "20", "21",
+            "22", "23", "24",
+            "25", "26", "27",
+            "28", "29", "30",
+            "31"
+        ],
+        "time": [
+            "00:00", "01:00", "02:00",
+            "03:00", "04:00", "05:00",
+            "06:00", "07:00", "08:00",
+            "09:00", "10:00", "11:00",
+            "12:00", "13:00", "14:00",
+            "15:00", "16:00", "17:00",
+            "18:00", "19:00", "20:00",
+            "21:00", "22:00", "23:00"
+        ],
+        "pressure_level": [
+            "1", "2", "3",
+            "5", "7", "10",
+            "20", "30", "50",
+            "70", "100", "125",
+            "150", "175", "200",
+            "225", "250", "300",
+            "350", "400", "450",
+            "500", "550", "600",
+            "650", "700", "750",
+            "775", "800", "825",
+            "850", "875", "900",
+            "925", "950", "975",
+            "1000"
+        ],
+        "data_format": "netcdf",
+        "download_format": "unarchived",
+        "grid": [2.0, 2.0]
+    }
+    filename = "era5_cloud_hourly_"+variable+"_2010"+month+".nc"
+    print(filename)
+    client.retrieve(dataset, request, filename)
+
+if __name__ == "__main__":
+    variables = ["fraction_of_cloud_cover", "specific_cloud_ice_water_content",
+                "specific_cloud_liquid_water_content", "specific_humidity"]
+    months = list(map(lambda x: str(x).zfill(2), range(1, 13)))
+    variables_all = [r[0] for r in itertools.product(variables, months)]
+    months_all = [r[1] for r in itertools.product(variables, months)]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        executor.map(download_era5, variables_all, months_all)
