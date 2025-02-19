a30814671a471eada9462cae9228eef21d779c69
Edward Speer <espeer@caltech.edu>
2025-02-18 20:38:20 -0800

    Add additional years of MODIS LAI data (#109)

--- modis_lai/create_artifacts.jl
+++ modis_lai/create_artifacts.jl
@@ -28,16 +28,23 @@ using ClimaArtifactsHelper
 ################################################################################
 
 # Dataset to be fetched via the GriddingMachine
-MODIS_LAI_DATASET = "MODIS_2X_1M_2008_V1"
+MODIS_LAI_DATASET_PREFIX = "MODIS_2X_1M_"
+MODIS_LAI_DATASET_SUFFIX = "_V1"
 
 # Output directory
 OUTPUT_DIR = "modis_lai"
 
 # Output nc file name
-OUTPUT_FILE = "Yuan_et_al_2011_1x1.nc"
+OUTPUT_FILE_PREFIX = "Yuan_et_al_"
 
-# Resolution of the simulation
+# Output nc file name suffix
+OUTPUT_FILE_SUFFIX = "_1x1.nc"
+
+# Output data resolution
 RESOLUTION = 1
+
+# Years to fetch
+YEARS = range(2000, 2020)
 
 ################################################################################
 # MAIN                                                                         #
@@ -51,71 +58,86 @@ else
     mkdir(OUTPUT_DIR)
 end
 
-# Download the LAI data as a julia artifact.
-lai_collector  = Collector.lai_collection()
-download_path = Collector.query_collection(lai_collector, MODIS_LAI_DATASET)
+for year in YEARS
+    # Dataset to be fetched via the GriddingMachine
+    MODIS_LAI_DATASET = MODIS_LAI_DATASET_PREFIX * string(year) *
+                         MODIS_LAI_DATASET_SUFFIX
 
-# Read the data from the dowloaded nc file
-lai_data = Indexer.read_LUT(download_path)
+    # Output nc file name
+    OUTPUT_FILE = OUTPUT_FILE_PREFIX * string(year) * OUTPUT_FILE_SUFFIX
 
-# Regrid the data to the desired simulation resolution - 1x1 degree. Note that 
-# the regridder will require integer trucation of the divider (1/R)
-lai_data_regridded = Blender.regrid(lai_data[1], Int64(1 / RESOLUTION))
+    # Download the LAI data as a julia artifact.
+    lai_collector  = Collector.lai_collection()
+    download_path = Collector.query_collection(lai_collector, MODIS_LAI_DATASET)
 
-# Get the original the lat/lon vectors of the dataset so that we may compute the
-# lat/lon of the regridded data.
-orig_data = NCDataset(download_path, "r")
-orig_lat = orig_data["lat"][:]
-orig_lon = orig_data["lon"][:]
-close(orig_data)
+    # Read the data from the dowloaded nc file
+    lai_data = Indexer.read_LUT(download_path)
 
-# Convert all NANs in the data to 0.0 (i.e, LAI should be 0 over the ocean)
-lai_data_regridded .= ifelse.(isnan.(lai_data_regridded), 0.0f0,
+    # Regrid the data to the desired simulation resolution - 1x1 degree. Note
+    # that the regridder will require integer trucation of the divider (1/R)
+    lai_data_regridded = Blender.regrid(lai_data[1], Int64(1 / RESOLUTION))
+
+    # Get the original the lat/lon vectors of the dataset so that we may compute
+    # the lat/lon of the regridded data.
+    orig_data = NCDataset(download_path, "r")
+    orig_lat = orig_data["lat"][:]
+    orig_lon = orig_data["lon"][:]
+    close(orig_data)
+
+    # Convert all NANs in the data to 0.0 (i.e, LAI should be 0 over the ocean)
+    lai_data_regridded .= ifelse.(isnan.(lai_data_regridded), 0.0f0,
                                                              lai_data_regridded)
 
-# Since we regridded to half of the resolution of the original data, we need to 
-# average every 2 lat/lon points to get the lat/lon of the regridded data.
-out_lon = (orig_lon[1:2:end] .+ orig_lon[2:2:end]) ./ 2
-out_lat = (orig_lat[1:2:end] .+ orig_lat[2:2:end]) ./ 2
+    # Since we regridded to half of the resolution of the original data, we need
+    # to average every 2 lat/lon points to get the lat/lon of the regridded
+    # data.
+    out_lon = (orig_lon[1:2:end] .+ orig_lon[2:2:end]) ./ 2
+    out_lat = (orig_lat[1:2:end] .+ orig_lat[2:2:end]) ./ 2
 
-# Write the regridded data to the output directory
-output_path = joinpath(OUTPUT_DIR, OUTPUT_FILE)
-ds          = NCDataset(output_path, "c")
+    # Write the regridded data to the output directory
+    output_path = joinpath(OUTPUT_DIR, OUTPUT_FILE)
+    ds          = NCDataset(output_path, "c")
 
-# Define the dimensions of the data -> 2D spatially varying data over 12 months
-defDim(ds, "lon", size(lai_data_regridded)[1])
-defDim(ds, "lat", size(lai_data_regridded)[2])
-defDim(ds, "time", 12)
+    # Define the dimensions of the data -> 2D spatially varying data over 12
+    # months
+    defDim(ds, "lon", size(lai_data_regridded)[1])
+    defDim(ds, "lat", size(lai_data_regridded)[2])
+    defDim(ds, "time", 12)
 
-# Define the variables of the data - lat, lon, and the lai
-la      = defVar(ds, "lat", Float32, ("lat",))
-lo      = defVar(ds, "lon", Float32, ("lon",))
-month   = defVar(ds, "time", Int32, ("time",))
-lai_var = defVar(ds, "lai", Float32, ("lon", "lat", "time"))
+    # Define the variables of the data - lat, lon, and the lai
+    la      = defVar(ds, "lat", Float32, ("lat",))
+    lo      = defVar(ds, "lon", Float32, ("lon",))
+    month   = defVar(ds, "time", Int32, ("time",))
+    lai_var = defVar(ds, "lai", Float32, ("lon", "lat", "time"))
 
-# Set the attributes of the variables.
-la.attrib["units"]             = "degrees_north"
-la.attrib["standard_name"]     = "latitude"
-lo.attrib["units"]             = "degrees_east"
-lo.attrib["standard_name"]     = "longitude"
-month.attrib["units"]          = "seconds since 1970-01-01"
-month.attrib["standard_name"]  = "time"
-month.attrib["calendar"]       = "proleptic_gregorian"
-lai_var.attrib["units"]         = "m^2 m^-2"
-lai_var.attrib["standard_name"] = "Leaf area index"
+    # Set the attributes of the variables.
+    la.attrib["units"]              = "degrees_north"
+    la.attrib["standard_name"]      = "latitude"
+    lo.attrib["units"]              = "degrees_east"
+    lo.attrib["standard_name"]      = "longitude"
+    month.attrib["units"]           = "seconds since 1970-01-01"
+    month.attrib["standard_name"]   = "time"
+    month.attrib["calendar"]        = "proleptic_gregorian"
+    lai_var.attrib["units"]         = "m^2 m^-2"
+    lai_var.attrib["standard_name"] = "Leaf area index"
 
-# Write the data for each variable out to the nc file
-la[:]     = out_lat
-lo[:]     = out_lon
-# Note that even spacing on the timestamps is required for the periodicCalendar
-# boundary condition
-times     = [DateTime(2008, 1, 1) + Dates.Day(30 * (k - 1)) for k in 1:12]
-month[:]  = Int32.([Dates.datetime2unix(t) for t in times])
-lai_var[:, :, :] = lai_data_regridded
-close(ds)
+    # Write the data for each variable out to the nc file
+    la[:]     = out_lat
+    lo[:]     = out_lon
+    
+    # Data from the gridding machhine is month (1:1:12), and we need to convert
+    # to DateTime format for the time variable so that we can use the
+    # periodicCalendar calendar boundary condition in the Land model.
+    # Note that even spacing on the timestamps is required for the
+    # periodicCalendar boundary condition
+    times     = [DateTime(year, 1, 1) + Dates.Day(30 * (k - 1)) for k in 1:12]
+    month[:]  = Int32.([Dates.datetime2unix(t) for t in times])
+    lai_var[:, :, :] = lai_data_regridded
+    close(ds)
 
-# Remove the initial downloaded artifact file - desired data is now stored in 
-# the CliMA artifact.
-Collector.clean_collections!(lai_collector)
+    # Remove the initial downloaded artifact file - desired data is now stored
+    # in the CliMA artifact.
+    Collector.clean_collections!(lai_collector)
+end
 
 create_artifact_guided(OUTPUT_DIR; artifact_name = basename(@__DIR__))
