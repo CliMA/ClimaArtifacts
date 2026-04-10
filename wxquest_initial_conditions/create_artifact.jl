f0f2169703540ceb8d2a3a0a3d9a118389ac63c2
Nat Efrat-Henrici <60049837+nefrathenrici@users.noreply.github.com>
2026-04-09 21:01:03 -0400

    Generate atmos target grid for wxquest_initial_conditions (#156)

--- wxquest_initial_conditions/create_artifact.jl
+++ wxquest_initial_conditions/create_artifact.jl
@@ -1,4 +1,5 @@
 using ClimaArtifactsHelper
+using NCDatasets
 
 dates_file = joinpath(@__DIR__, "dates.txt")
 output_dir = "wxquest_initial_conditions_artifact"
@@ -24,20 +25,38 @@ mktempdir(prefix="weatherquest_") do repo_dir
         @info "  $date $time"
         run(`python3 $get_ic
             --date $date --time $time
+            --atmos-levels model
             --output-dir $output_dir`)
     end
 
     @info "Instantiating WeatherQuest Julia environment..."
     run(`julia --project=$repo_dir -e "using Pkg; Pkg.instantiate()"`)
 
-    @info "Preprocessing initial conditions..."
     abs_output_dir = abspath(output_dir)
+
+    @info "Generating atmosphere target grid file..."
+    atmos_grid_file = joinpath(abs_output_dir, "atmos_grid.nc")
+    NCDataset(atmos_grid_file, "c") do ds
+        defDim(ds, "lon", 384)
+        defDim(ds, "lat", 192)
+        lon_var = defVar(ds, "lon", Float32, ("lon",),
+            attrib = ["units" => "degrees_east", "axis" => "X",
+                      "standard_name" => "longitude", "long_name" => "Longitude"])
+        lat_var = defVar(ds, "lat", Float32, ("lat",),
+            attrib = ["units" => "degrees_north", "axis" => "Y",
+                      "standard_name" => "latitude", "long_name" => "Latitude"])
+        lon_var[:] = Float32.(range(-180, 180, length = 384))
+        lat_var[:] = Float32.(range(-90, 90, length = 192))
+    end
+
+    @info "Preprocessing initial conditions..."
     cd(joinpath(repo_dir, "processing")) do
         run(`julia --project=$repo_dir preprocessing.jl
             --start-datetime 20100101_0000
             --end-datetime 20101001_0000
             --interval 3months
-            --data-dir $abs_output_dir`)
+            --data-dir $abs_output_dir
+            --atmos-grid-file $atmos_grid_file`)
     end
 end
 
